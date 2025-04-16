b91dc819e05c40da8836ac65db0e01534c562fc4
Vladislav Sumin <vladislavsumin@mail.ru>
2025-04-16 19:20:54 +0300

    add lib for fs utils

---
+++ lib/libfs.lua
@@ -0,0 +1,24 @@
+local fs = require("filesystem")
+
+--- Полностью читает файл в строку.
+--- Возврашщает или данные ввиде строки, или ошибку вторым возвращаемым значением.
+--- @param path string путь к файлу.
+--- @return string?, string? - data, error
+function fs.readFileToString(path)
+    fileHandler, error = fs.open(path, "r")
+    if not fileHandler then
+        return nil, error
+    end
+
+    local data = ""
+    repeat
+        local read = fileHandler:read(math.maxinteger)
+        data = data .. (read or "")
+    until not read
+
+    fileHandler:close()
+
+    return data, nil
+end
+
+return fs
