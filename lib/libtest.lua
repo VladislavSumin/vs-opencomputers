f728e2db7933fc9427148bd5b5d1e6f6b77d0faa
Vladislav Sumin <vladislavsumin@mail.ru>
2025-04-16 21:45:24 +0300

    update libtest error stack

--- lib/libtest.lua
+++ lib/libtest.lua
@@ -3,14 +3,15 @@
 local libtest = {}
 
 function libtest.assertEquals(expected, actual)
-    assert(
-        expected == actual,
-        "Equals assertion failed:\n\tExpected: " .. expected .. "\n\tActual: " .. actual
-    )
+    if expected ~= actual then
+        -- level = 2 нужен что бы в стеке было место вызова этой функции а не сама эта функция.
+        error("Equals assertion failed:\n\tExpected: " .. expected .. "\n\tActual: " .. actual, 2)
+    end
 end
 
 return function()
     for name, method in pairs(libtest) do
+        -- Регистрируем методы в глобальной таблице что бы упростить синтаксис их вызова.
         _G[name] = method
     end
 end
