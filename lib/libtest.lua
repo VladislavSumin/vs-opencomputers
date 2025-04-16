b500b0581505612f6f3371354603e236803ec8b4
Vladislav Sumin <vladislavsumin@mail.ru>
2025-04-16 19:44:09 +0300

    fix lib test to directly inject test methods in global table.

--- lib/libtest.lua
+++ lib/libtest.lua
@@ -1,8 +1,16 @@
 --- Набор полезных расширений для unit тестирования.
 
-function assertEquals(expected, actual)
+local libtest = {}
+
+function libtest.assertEquals(expected, actual)
     assert(
         expected == actual,
         "Equals assertion failed:\n\tExpected: " .. expected .. "\n\tActual: " .. actual
     )
+end
+
+return function()
+    for name, method in pairs(libtest) do
+        _G[name] = method
+    end
 end
